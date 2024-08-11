e41420c14554ec116a0fe631bd674b69df298049
Ricardo Jacques <ricardojacques03@gmail.com>
2024-08-11 20:45:29 -0300

    Initial commit

---
+++ Models/item.cs
@@ -0,0 +1,16 @@
+﻿namespace SiteMVC.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public string Proprietario { get; set; }
+        public double Valor { get; set; }
+        public string Estado { get; set; }
+        public Item()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using SiteMVC.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>
+    (options => options .UseSqlServer(
+        "Data Source=localhost\\SQLEXPRESS;Initial Catalog=escoteiros_db;Integrated Security=True;Encrypt=True;Trust Server Certificate=True"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
