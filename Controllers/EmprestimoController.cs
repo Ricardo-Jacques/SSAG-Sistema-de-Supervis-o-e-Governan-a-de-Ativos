200b089883dfdf305351dc5b998c87ef11901105
Ricardo Jacques <ricardojacques03@gmail.com>
2024-08-18 19:34:19 -0300

    Validações do empréstimo
    Criadas as validações para que o formulário de empréstimo não crie solicitações de itens que já estão emprestados ou que não existem.

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SiteMVC.Models;
 using System.Data.SqlClient;
 
@@ -28,37 +29,51 @@ namespace SiteMVC.Controllers
         [HttpPost]
         public IActionResult CriarEmprestimo(Emprestimo emprestimo)
         {
-            using (SqlConnection conexao = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=escoteiros_db;Integrated Security=True;Encrypt=True;TrustServerCertificate=True"))
+            var itemExistente = _context.Items.FirstOrDefault(i =>
+            i.Nome == emprestimo.Item &&
+            i.Tipo == emprestimo.TipoItem &&
+            i.Id == emprestimo.IdItem);
+
+            if (itemExistente != null)
             {
-                // Abre a conexão
-                conexao.Open();
-
-                // Obtém o nome e o ID do usuário logado
-                string usuario = User.Identity.Name;
-                string idUsuario = User.FindFirst("UserId")?.Value;
-
-                // Captura os dados do formulário
-                string item = emprestimo.Item;
-                string tipoItem = emprestimo.TipoItem;
-                int idItem = emprestimo.IdItem;
-
-                // Cria uma nova solicitação
-                var query = "INSERT INTO solicitacoes (usuario, idUsuario, item, tipoItem, idItem, status) " +
-                            "VALUES (@usuario, @idUsuario, @item, @tipoItem, @idItem, 'Aguardando aprovação')";
-
-                using (SqlCommand comando = new SqlCommand(query, conexao))
+                // A combinação existe na tabela items
+                using (SqlConnection conexao = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=escoteiros_db;Integrated Security=True;Encrypt=True;TrustServerCertificate=True"))
                 {
-                    // Definindo os parâmetros
-                    comando.Parameters.AddWithValue("@usuario", usuario);
-                    comando.Parameters.AddWithValue("@idUsuario", idUsuario);
-                    comando.Parameters.AddWithValue("@item", item);
-                    comando.Parameters.AddWithValue("@tipoItem", tipoItem);
-                    comando.Parameters.AddWithValue("@idItem", idItem);
+                    // Abre a conexão
+                    conexao.Open();
 
-                    comando.ExecuteNonQuery();
+                    // Obtém o nome e o ID do usuário logado
+                    string usuario = User.Identity.Name;
+                    string idUsuario = User.FindFirst("UserId")?.Value;
+
+                    // Captura os dados do formulário
+                    string item = emprestimo.Item;
+                    string tipoItem = emprestimo.TipoItem;
+                    int idItem = emprestimo.IdItem;
+
+                    // Cria uma nova solicitação
+                    var query = "INSERT INTO solicitacoes (usuario, idUsuario, item, tipoItem, idItem, status) " +
+                                "VALUES (@usuario, @idUsuario, @item, @tipoItem, @idItem, 'Aguardando aprovação')";
+
+                    using (SqlCommand comando = new SqlCommand(query, conexao))
+                    {
+                        // Definindo os parâmetros
+                        comando.Parameters.AddWithValue("@usuario", usuario);
+                        comando.Parameters.AddWithValue("@idUsuario", idUsuario);
+                        comando.Parameters.AddWithValue("@item", item);
+                        comando.Parameters.AddWithValue("@tipoItem", tipoItem);
+                        comando.Parameters.AddWithValue("@idItem", idItem);
+
+                        comando.ExecuteNonQuery();
+                    }
+
+                    return RedirectToAction("Index");
                 }
 
-                return RedirectToAction("Index"); 
+            }
+            else
+            {
+                return RedirectToAction("Index");
             }
         }
     }
---
+++ wwwroot/js/emprestimo.js
@@ -0,0 +1,62 @@
+﻿function filtrarItens() {
+    var tipoSelecionado = document.getElementById("tipoItem").value;
+    var itemDesejado = document.getElementById("itemDesejado");
+    var itens = itemDesejado.options;
+
+    for (var i = 0; i < itens.length; i++) {
+        var tipoItem = itens[i].getAttribute("data-tipo");
+
+        if (tipoSelecionado === "" || tipoItem === tipoSelecionado) {
+            itens[i].style.display = "block";
+        } else {
+            itens[i].style.display = "none";
+        }
+    }
+
+    itemDesejado.selectedIndex = 0;
+}
+
+function filtrarIds() {
+    var itemSelecionado = document.getElementById("itemDesejado").value;
+    var idItem = document.getElementById("idItem");
+    var ids = idItem.options;
+
+    for (var i = 0; i < ids.length; i++) {
+        var nomeItem = ids[i].getAttribute("data-item");
+
+        if (itemSelecionado === "" || nomeItem === itemSelecionado) {
+            ids[i].style.display = "block";
+        } else {
+            ids[i].style.display = "none";
+        }
+    }
+
+    idItem.selectedIndex = 0;
+}
+
+function validarFormulario() {
+    // Validação de tipo
+    var tipoSelecionado = document.getElementById("tipoItem").value;
+    if (tipoSelecionado === "") {
+        alert("Por favor, selecione um tipo.");
+        return false; // Impede o envio do formulário
+    }
+
+    // Validação de item
+    var itemSelecionado = document.getElementById("itemDesejado").selectedOptions[0];
+    var estadoItem = itemSelecionado.getAttribute("data-estado");
+
+    if (estadoItem === "Emprestado") {
+        alert("O item selecionado está emprestado. Por favor, escolha um item disponível.");
+        return false; // Impede o envio do formulário
+    }
+
+    // Validação de ID
+    var idSelecionado = document.getElementById("idItem").value;
+    if (idSelecionado === "") {
+        alert("Por favor, selecione um ID.");
+        return false; // Impede o envio do formulário
+    }
+
+    return true; // Permite o envio do formulário se todas as validações forem bem-sucedidas
+}
