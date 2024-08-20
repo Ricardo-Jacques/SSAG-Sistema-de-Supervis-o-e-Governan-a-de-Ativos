bb8a5818b7c93e7cf4a0dde3aa2089cecc7ceaa5
Luis Gustavo F Freire <holyfreire@gmail.com>
2024-08-20 01:18:43 -0300

    criação da tabelas.css
    melhorei as tabelas em geral

--- obj/Debug/net8.0/SiteMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/SiteMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7aaca9c185846167290e7abe693476d29dc9c58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08422bb7e07a1980f0f508646557afa88e97224")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
